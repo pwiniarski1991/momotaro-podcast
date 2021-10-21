cebdd31d8c0e0518810ce09a4400437f37d683fe
Piotr <piotr.winiarski@300brains.com>
2021-10-21 16:30:15 +0200

    setup Error Boundary component and use in components tree

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,8 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
